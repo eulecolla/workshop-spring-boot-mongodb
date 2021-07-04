8102b6c067f6d6044969d9a7cbf3731f3f672731
Eule Hoelz Colla <ecolla@netpoint.com.br>
2021-07-04 15:26:48 -0300

    Obtendo um usuário por id

--- src/main/java/com/eulecorp/workshopmongo/resources/UserResource.java
+++ src/main/java/com/eulecorp/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,28 +24,16 @@ public class UserResource {
 
 	@RequestMapping(method = RequestMethod.GET)
 // OU PODE USAR SIMPLESMENTE @GetMapping
-//	public List<User> findAll(){
-//		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-//		User alex = new User("2", "Alex Green", "alex@gmail.com");
-//		List<User> list = new ArrayList<>();
-//		list.addAll(Arrays.asList(maria, alex));
-//		return list;
-//	}
-// Para encapsular toda a resposta com possíveis cabeçalhos, body, erros, etc. tem que usar o 	
-// ResponseEntity<T>
-// e mudar o return com as infos  adicionais necessárias
-//	public ResponseEntity<List<User>> findAll(){
-//		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-//		User alex = new User("2", "Alex Green", "alex@gmail.com");
-//		List<User> list = new ArrayList<>();
-//		list.addAll(Arrays.asList(maria, alex));
-//		return ResponseEntity.ok().body(list);
-//	}
-	
+// Para encapsular toda a resposta com possíveis cabeçalhos, body, erros, etc. tem que usar o ResponseEntity<T>
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 }
