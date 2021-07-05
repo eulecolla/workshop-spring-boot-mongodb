bdfdbe76d486fd742262babe9da8c2ae0851c161
Eule Hoelz Colla <ecolla@netpoint.com.br>
2021-07-05 20:56:34 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/eulecorp/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.eulecorp.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eulecorp.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	
+
+}
